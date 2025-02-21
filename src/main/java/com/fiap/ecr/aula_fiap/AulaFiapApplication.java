7fbf4df76049c043e8bfca80005cc68cdf8477cd
Luiz <gutomelki2016@gmail.com>
2025-02-21 20:31:30 -0300

    Aula Java - Iniciando com Spring Boot

---
+++ src/main/java/com/fiap/ecr/aula_fiap/AulaFiapApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.ecr.aula_fiap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaFiapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaFiapApplication.class, args);
+	}
+
+}
